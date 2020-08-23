8d953f4f19600d21ca8738a8925150f681cad7cc
Jannik Buhr <jannik.m.buhr@gmail.com>
2020-08-23 15:25:39 +0200

    Add runApp addin

--- R/addins.R
+++ R/addins.R
@@ -13,3 +13,42 @@
 stopServer <- function(){
   servr::daemon_stop(1)
 }
+
+#' convertCitation
+#'
+#' Convert a citr citation into a RefManageR cition to be
+#' used in Xaringan slides.
+#'
+#' @return
+#' NULL
+#' @export
+convertCitation <- function() {
+  x <- rstudioapi::getActiveDocumentContext()
+  selection <- rstudioapi::primary_selection(x)
+  cit <- stringr::str_extract(selection$text, "(?<=@)[^\\]\\s]+")
+  result <- paste0("`r Citet(bib, \"", cit,"\")`")
+  rstudioapi::insertText(selection$range, result)
+}
+
+#' runApp
+#'
+#' Run a shinyApp packaged with golem in developement
+#'
+#' @return
+#' NULL
+#' @export
+runApp <- function() {
+  file <- file.path(here::here(), "app.R")
+  if (file.exists(file)) {
+    rstudioapi::documentSaveAll()
+    devtools::document()
+    pkgload::load_all()
+    shiny::runApp(appDir = here::here())
+  } else {
+    usethis::ui_todo("Unable to locate app.R file.\n
+                     Create a file app.R in the root directory
+                     of your golem shiny app package and make sure it
+                     contains:")
+    usethis::ui_code_block("# ShinyApp\nrun_app()")
+  }
+}
